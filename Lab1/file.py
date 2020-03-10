c881db7bb8bf92aed88672466f2a7cbe808ba555
Chobotov <chobotov@bk.ru>
2020-03-10 20:43:46 +0300

    Second Commit

---
+++ Lab1/file.py
@@ -0,0 +1,15 @@
+cortej = ('1','2','3','4','5','6','7','8','9','10')
+dictionary = {}
+
+
+def add(i):
+    dictionary[i] = i ** 2
+
+
+for i in range(1, 50):
+    cortej
+    if i % 2 == 0:
+        add(i)
+
+print(cortej)
+print(dictionary)
